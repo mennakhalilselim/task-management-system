cd6660786659d9a6e59180879c45b811cc4da159
Menna Selim <mennakhalilselim@gmail.com>
2025-10-23 19:04:41 +0300

    delete builder from login, register requests & delete bycrypt from sample user password

--- src/main/java/io/github/mennakhalilselim/taskmanagementsystem/model/dto/request/LoginRequest.java
+++ src/main/java/io/github/mennakhalilselim/taskmanagementsystem/model/dto/request/LoginRequest.java
@@ -3,12 +3,10 @@ package io.github.mennakhalilselim.taskmanagementsystem.model.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequest {
